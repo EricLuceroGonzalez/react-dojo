0555cacb2df2c2a8856602f2f7a3b198630d9430
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-09 21:59:12 -0500

    All

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HolaMundo from "./Components/HolaMundo";
 
 function App() {
   return (
     <React.Fragment>
       <h1>Hola mundo!</h1>
       <p>Lorem ipsum dolor</p>
+      <HolaMundo/>
     </React.Fragment>
   );
 }
---
+++ src/Components/HolaMundo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HolaMundo extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h1>Hola desde componente HolaMundo.js</h1>
+          );
+    }
+}
+ 
+export default HolaMundo;
