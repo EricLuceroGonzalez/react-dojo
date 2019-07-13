46d28e6732f199c658a9d078f075aa615f01846e
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-12 20:11:02 -0500

    setting up - Bootstrap and ReactStrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import HolaMundo from "./Components/HolaMundo";
 
 function App() {
   return (
     <React.Fragment>
       <h1>Hola mundo!</h1>
       <p>Lorem ipsum dolor</p>
-      <HolaMundo/>
     </React.Fragment>
   );
 }
