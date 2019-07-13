091eabcbb1d2dfd5dfaa6bffbd7eb53571eb3f98
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-12 22:05:16 -0500

    Contadores ready2

--- src/Components/Contadores.js
+++ src/Components/Contadores.js
@@ -2,12 +2,27 @@ import React, { Component } from "react";
 import Contador from "./Contador";
 
 class Contadores extends Component {
-  state = {};
+  state = {
+    contadores: [
+      { id: 0, titulo: "Alpha", valor: 0 },
+      { id: 1, titulo: "Bravo", valor: 3 },
+      { id: 2, titulo: "Charlie", valor: -3 },
+      { id: 3, titulo: "Delta", valor: 0 },
+      { id: 4, titulo: "Echo", valor: 3 }
+    ]
+  };
   render() {
     return (
       <React.Fragment>
-        <Contador titulo="Beta" valor={5} />
-        <Contador titulo="Gama" valor={0} />
+        {this.state.contadores.map(contador => {
+          return (
+            <Contador
+              key={contador.id}
+              titulo={contador.titulo}
+              valor={contador.valor}
+            />
+          );
+        })}
       </React.Fragment>
     );
   }
