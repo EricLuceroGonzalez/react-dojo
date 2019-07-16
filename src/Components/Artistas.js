6d67c0388b9bbfedd91fec5cbdafbbff253a42bf
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-15 21:09:40 -0500

    get method

--- src/Components/Artistas.js
+++ src/Components/Artistas.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Artistas extends Component {
+  state = {};
+  componentDidMount() {
+    // Build the API
 
-    /* Build the API */
-    
-    state = {  }
-    render() { 
-        return (  
-            <h1>Lista de Artistas</h1>
-        );
-    }
+    // **** GET: Make a request for a user with a given ID
+    axios
+      .get("https://devf-musica.herokuapp.com/api/v1/artista")
+      .then(response => {
+        console.log(response.data);
+        // Pass the data
+        this.setState({
+            artistas: response.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }
+  render() {
+    return <h1>Lista de Artistas</h1>;
+  }
 }
- 
+
 export default Artistas;
